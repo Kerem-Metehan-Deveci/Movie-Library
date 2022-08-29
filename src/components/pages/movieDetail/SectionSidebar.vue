382e2f08c8d3d483f8be022755756cd9abc7d0a5
Kerem Metehan Deveci <keremdeveci100@gmail.com>
2022-08-29 15:44:18 +0300

    Add Go Back Button

--- src/components/pages/movieDetail/SectionSidebar.vue
+++ src/components/pages/movieDetail/SectionSidebar.vue
@@ -1,14 +1,28 @@
 <script setup>
 import { computed } from '@vue/reactivity';
+import { ArrowBackOutline } from '@vicons/ionicons5'
+import { useRouter } from 'vue-router';
 
 const props = defineProps({
     image: { type: String, default: null },
     rating: { type: Number, default: 0 },
     genreList: { type: Array, default: () => [] },
 });
+const router = useRouter();
+const back =() => {
+    router.go(-1)
+}
 </script>
 
 <template>
+    <div class="mb-3">
+        <n-button quaternaryvis @click="back">
+            <template #icon>
+                <n-icon> <arrow-back-outline /> </n-icon>
+            </template>
+            <span class="mt-1 fw-bold">Back</span>
+        </n-button>
+    </div>
     <div>
         <n-image width="100" :img-props="{ class: 'w-100 rounded' }"
             :src="props.image"
